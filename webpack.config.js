8fd2560606adc1506b4dcd1bef701b60fdd98cc2
Victor Melo <melo.victor86@gmail.com>
2025-03-18 13:50:40 -0300

    corrected entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './chicorei-teste/webpack.config.js', // Update this path
+  entry: './src/index.js', // Entry
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
